9c8f1b60d5cb3dc9ee3b26be8e4373c80200ee24
Betcy Babu <aleenababu93@gmail.com>
2021-12-03 11:38:24 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+
+default = "ap-south-1"
+
+}
+
+variable "seckey" {
+
+    default = "Enter your secret_key"
+}
+
+variable "accesskey" {
+
+    default = "Enter your access_key"
+}
+
+variable "type" {
+
+    default = "t2.micro"
+
+}
+
+variable "ami"{
+   default = "ami-052cef05d01020f1d"
+}
